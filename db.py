13b8457fb7503842e4dd56b8e55d7ba83c75f5bc
Hetal Mangukia <hmangukia9@gmail.com>
2025-07-16 12:39:58 -0400

    ci: added steps to use a postgres service for tests, added .env file to use DATABASE_URL

--- db.py
+++ db.py
@@ -1,6 +1,14 @@
 from sqlmodel import create_engine, Session
+import os
+from dotenv import load_dotenv
 
-engine = create_engine(f'postgresql://hetalmangukia:postgres@localhost:5432/postgres')
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
+
+engine = create_engine(DATABASE_URL)
 
 def get_session():
     with Session(engine) as session:
