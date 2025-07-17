ed59ee513bc1a217bca330a2118ff91003db639d
Hetal Mangukia <hmangukia9@gmail.com>
2025-07-17 15:54:25 -0400

    chore: made some db changes, use default_factory, make slug and long_url non nullable, make long_url unique

--- models.py
+++ models.py
@@ -2,24 +2,28 @@ from sqlmodel import SQLModel, Field
 from datetime import datetime
 from pydantic import BaseModel
 
+
 class URL(SQLModel, table=True):
     id: int | None = Field(primary_key=True, default=None)
-    slug: str = Field(index=True, unique=True)
-    long_url: str
-    created_at: datetime = Field(default=datetime.utcnow())
+    slug: str = Field(index=True, unique=True, nullable=False)
+    long_url: str = Field(index=True, unique=True, nullable=False)
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+
 
 class Visits(SQLModel, table=True):
     id: int | None = Field(primary_key=True, default=None)
     url_id: int = Field(foreign_key="url.id")
-    visit_time: datetime = Field(default=datetime.utcnow())
+    visit_time: datetime = Field(default_factory=datetime.utcnow)
 
 
 class ShortenUrlRequest(BaseModel):
     long_url: str
 
+
 class ShortenUrlResponse(BaseModel):
     short_url: str
     slug: str
+
 
 class URLStatsResponse(BaseModel):
     slug: str
