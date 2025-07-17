0485f597dd5a81cc097868c98535fd920e16e240
Hetal Mangukia <hmangukia9@gmail.com>
2025-07-17 15:55:41 -0400

    refactor: log visit of a url using a background task

--- api/routes/redirect.py
+++ api/routes/redirect.py
@@ -3,22 +3,30 @@ from db import get_session
 from sqlmodel import Session, select
 from fastapi import Depends, APIRouter, HTTPException
 from fastapi.responses import RedirectResponse
-from datetime import datetime
+from fastapi import BackgroundTasks
 
 router = APIRouter()
 
+
 @router.get("/{slug}")
-def redirect(slug: str, session: Session = Depends(get_session)):
+async def redirect(
+    slug: str,
+    background_tasks: BackgroundTasks,
+    session: Session = Depends(get_session),
+):
     # checks if the slug exists in the database. If not, raise a 404 error
     url = session.exec(select(URL).where(slug == URL.slug)).first()
     if not url:
         raise HTTPException(status_code=404, detail="URL not found")
 
     # logs the visit to the database
-    visit = Visits(url_id=url.id)
-    session.add(visit)
-    session.commit()
-    session.refresh(visit)
-    
+    background_tasks.add_task(log_visit, url.id, session)
+
     # redirects the user to the original long URL
     return RedirectResponse(url.long_url, status_code=307)
+
+
+def log_visit(url_id: int, session: Session):
+    visit = Visits(url_id=url_id)
+    session.add(visit)
+    session.commit()
